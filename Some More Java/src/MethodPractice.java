c20f306daf685b69f21082b096e8f78983dd945d
mhs <mhs@MHS-Science3-17>
2017-10-25 17:10:33 -0700

    Help

---
+++ Some More Java/src/MethodPractice.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author mhs
+ *
+ */
+public class MethodPractice {
+public static void main(String []args){
+}
+public double max(double a,double b){ 
+	if(a>b){
+		return(b);}
+	else{
+		return(a);}
+
+	}
+}
